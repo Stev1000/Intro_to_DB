c3d8e882e096b7dcf49e74bb54ae2b31fc859f4f
Stevo <nsanzestevo1@gmail.com>
2024-10-23 19:20:39 +0200

    prints the full description of the table books from the database

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,4 @@
--- Use the correct database
-USE alx_book_store;
-
 -- task_4.sql
-SHOW CREATE TABLE books;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
